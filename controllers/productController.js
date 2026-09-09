8d4cc41f941a72735983a9454039d61ef17b1703
Basit Ali <basitaibhatti57@gmail.com>
2026-09-09 22:53:35 +0500

    Add the PATCH & DELETE of the Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -75,4 +75,70 @@ const getProductById = async(req, res) =>{
     }
 }
 
-module.exports = { createProduct, getAllProducts, getProductById };
+const updateProductById = async(req, res) => {
+    const {name, description, price, stock, category_id} = req.body;
+    const {id} = req.params;
+    if (!name || !price || !stock || !category_id) {
+        return res.status(400).json({ message: 'Please provide all required fields' });
+    }
+
+    if (isNaN(price) || isNaN(stock)) {
+        return res.status(400).json({ message: 'Price and stock must be numbers' });
+    }
+
+    if (price <= 0 || stock < 0) {
+        return res.status(400).json({ message: 'Price and stock must be non-negative' });
+    }
+
+    try{
+        const existingProduct = await pool.query('SELECT * FROM products WHERE id = $1', [id]);
+        if(existingProduct.rowCount === 0){
+            return res.status(404).json({
+                message : "The product is not exist"
+            })
+        }
+        const existingCategory = await pool.query('SELECT * FROM categories WHERE id = $1', [category_id]);
+        if(existingCategory.rowCount === 0){
+            return res.status(404).json({
+                message : "Invalid category_id — category does not exist"
+            })
+        }
+
+        const updatedProduct = await pool.query(
+            'UPDATE products SET name = $1, description = $2, price = $3, stock = $4, category_id = $5 WHERE id = $6 RETURNING *',
+            [name, description, price, stock, category_id, id]
+        );
+        return res.status(200).json(updatedProduct.rows[0]);
+    }catch(error){
+        console.log("Error Updating Product by Id", error);
+        return res.status(500).json({
+            message : "Internal Server Error"
+        })
+    }
+}
+
+const deleteProduct = async(req, res) => {
+    const {id} = req.params;
+
+    try{
+        const existingProduct = await pool.query('SELECT * FROM products WHERE id = $1', [id]);
+        if(existingProduct.rowCount === 0){
+            return res.status(404).json({
+                message : "The product is not exist"
+            })
+        }
+
+        await pool.query('DELETE FROM products WHERE id = $1', [id]);
+        return res.status(200).json({
+            message : "Product deleted successfully"
+        });
+
+    }catch(error){
+        console.log("Error : Delete the Product ", error);
+        return res.status(500).json({
+            message : "Internal Server Error"
+        });
+    }
+}
+
+module.exports = { createProduct, getAllProducts, getProductById, updateProductById, deleteProduct };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {createProduct, getAllProducts, getProductById} = require('../controllers/productController');
+const {createProduct, getAllProducts, getProductById, updateProductById, deleteProduct} = require('../controllers/productController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/', authMiddleware, createProduct);
 router.get('/', authMiddleware, getAllProducts)
 router.get('/:id', getProductById);
+router.patch('/:id',authMiddleware, updateProductById)
+router.delete('/:id', authMiddleware, deleteProduct);
 module.exports = router;
